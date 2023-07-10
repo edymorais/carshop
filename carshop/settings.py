a507caa3cb4d4a8323465092d563a8983abe5f3d
edywascellys <edywascellys@gmail.com>
2023-07-10 11:00:38 -0300

    configurado CORS

--- carshop/settings.py
+++ carshop/settings.py
@@ -48,7 +48,11 @@ REST_FRAMEWORK = {
     ),
 }
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
